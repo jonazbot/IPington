3256d55e38c4f3992621ef727a25f49e1d36463c
jonas <jonasvalen@gmail.com>
2022-06-23 14:35:00 +0200

    Fix: ipington.py
    Remove redundant attribute IPington::`prefix`
    Fix prefix calls to super
    Add logging
    Remove traceback
    Fix encapsulation
    
    Bug bad naming in global namespace left over from refactoring

--- ipington.py
+++ ipington.py
@@ -37,8 +37,7 @@ class IPington(commands.Bot):
                  path_to_server: str,
                  path_to_jar: str,
                  server_port: str):
-        self.prefix = command_prefix
-        super(IPington, self).__init__(command_prefix=self.prefix)
+        super(IPington, self).__init__(command_prefix=command_prefix)
         self.minecraft_version = server_version
         self.server_path = path_to_server
         self.jar_path = path_to_jar
@@ -58,15 +57,15 @@ class Functions(commands.Cog):
     Attributes
     -----------
     bot
-        The bot is the :class: `discord.commands.Bot` or any subclass (like IPington)
+        The bot is the :class: `discord.commands.Bot.IPington`.
         which to register this group of commands.
     """
-    def __init__(self, bot: commands.Bot, logger: logging):
+    def __init__(self, bot: IPington, logger: logging):
         self.bot = bot
         self.logger = logger
 
     @staticmethod
-    def find_server_ip() -> str:
+    def _find_server_ip() -> str:
         """
         Find the current server IP address.
         """
@@ -94,10 +93,10 @@ class Functions(commands.Cog):
         Ask IPington to list the current bot commands.
         """
         cmd = '**Commands**       ***Description*** \n' \
-              f'`{bot.prefix}Info`         *Ask IPington to list the bot commands.*\n' \
-              f'`{bot.prefix}IP`           *Ask IPington to leak the current Minecraft server IP.*\n' \
-              f'`{bot.prefix}Version`      *Ask IPington to leak the current Minecraft server version.*\n' \
-              f'`{bot.prefix}Minecraft`    *Ask IPington to start the Minecraft server.*'
+              f'`{self.bot.command_prefix}Info`         *Ask IPington to list the bot commands.*\n' \
+              f'`{self.bot.command_prefix}IP`           *Ask IPington to leak the current Minecraft server IP.*\n' \
+              f'`{self.bot.command_prefix}Version`      *Ask IPington to leak the current Minecraft server version.*\n' \
+              f'`{self.bot.command_prefix}Minecraft`    *Ask IPington to start the Minecraft server.*'
         await ctx.send(cmd)
 
     @commands.command(name='IP')
@@ -106,18 +105,17 @@ class Functions(commands.Cog):
         Ask IPington to leak the current Minecraft server IP.
         """
         try:
-            await ctx.send(f'Minecraft server address:\n{self.find_server_ip()}:{bot.server_port}')
-        except Exception as e:
+            await ctx.send(f'Minecraft server address:\n{self._find_server_ip()}:{self.bot.server_port}')
+        except Exception:
             await ctx.send('IP service is currently not available.')
-            import traceback
-            traceback.print_exception(e)
+            self.logger.exception('IP address leak failed')
 
     @commands.command(name='Version')
     async def mc_version(self, ctx):
         """
         Ask IPington to leak the current Minecraft server version.
         """
-        await ctx.send(f'Current minecraft server version: {bot.minecraft_version}')
+        await ctx.send(f'Current minecraft server version: {self.bot.minecraft_version}')
 
     @commands.command(name='Minecraft')
     async def minecraft(self, ctx):
@@ -130,27 +128,27 @@ class Functions(commands.Cog):
             .. note:: The bot will only start the server after determining that
             it is not currently running.
         """
-        if self.is_server_running():
+        if self._is_server_running():
             await ctx.send('Minecraft server is already running.')
         else:
             await ctx.send('Starting Minecraft server...')
             try:
-                if os.path.exists(bot.server_path):
-                    if os.path.exists(bot.jar_path):
+                if os.path.exists(self.bot.server_path):
+                    if os.path.exists(self.bot.jar_path):
                         subprocess.call(
-                            f'cd {bot.server_path}'  # Change into the server directory
-                            f' && nohup java - Xms2G - Xmx8G - jar {bot.jar_path}'  # Start the server quietly 
+                            f'cd {self.bot.server_path}'  # Change into the server directory
+                            f' && nohup java - Xms2G - Xmx8G - jar {self.bot.jar_path}'  # Start the server quietly 
                             ' & > / dev / null &',  # Direct server output to garbage collection
                             shell=True)  # This option introduces a code injection vulnerability to the system.
-                        await ctx.send(f'Minecraft server is ready @ {self.find_server_ip()}')
+                        await ctx.send(f'Minecraft server is ready @ {self._find_server_ip()}')
                     else:
                         raise FileNotFoundError
                 else:
                     raise NotADirectoryError
-            except Exception as e:
-                await ctx.send('Failed to start Minecraft server!')
-                import traceback
-                traceback.print_exception(e)
+            except Exception:
+                msg = 'Failed to start Minecraft server!'
+                await ctx.send(msg)
+                self.logger.exception(msg)
 
 
 if __name__ == '__main__':
@@ -158,11 +156,11 @@ if __name__ == '__main__':
     config = dotenv_values('.conf')
 
     # Setup IPington.
-    bot: IPington = IPington(command_prefix=config['PREFIX'],
-                             server_version=config['MINECRAFT_VERSION'],
-                             path_to_server=config['PATH_TO_SERVER'],
-                             path_to_jar=config['PATH_TO_JAR'],
-                             server_port=config['SERVER_PORT'])
+    ipington: IPington = IPington(command_prefix=config['PREFIX'],
+                                  server_version=config['MINECRAFT_VERSION'],
+                                  path_to_server=config['PATH_TO_SERVER'],
+                                  path_to_jar=config['PATH_TO_JAR'],
+                                  server_port=config['SERVER_PORT'])
 
     # Run IPington
-    bot.run(config['TOKEN'])
+    ipington.run(config['TOKEN'])
